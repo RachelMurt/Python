254ca8d809e78d7e61ef411093f1a98e8f029e2a
Rachel Murtagh <56913574+RachelMurt@users.noreply.github.com>
2020-01-10 09:54:16 +0000

    Update and rename 1 adding two numbers together.py to User Defined

--- 1 adding two numbers together.py
+++
@@ -1,9 +0,0 @@
-#add two variables assign values to them
-numb1 = 14
-numb2 = 8
-
-#we need another variable called ans, we will assign the calculation to it
-
-ans = numb1 + numb2
-
-print  ("The answer is" ,ans)
