720d2ab6f0ad3e5b2188bb519adb3d7348383f31
Rachel Murtagh <56913574+RachelMurt@users.noreply.github.com>
2020-01-10 10:06:32 +0000

    Update and rename 1Star Wars .py to Star WarsV2 .py

---
+++ Star WarsV2 .py
@@ -0,0 +1,69 @@
+import csv
+
+file = open("SWMovies.csv", "w")
+newrecord ="Episode IV, A New Hope, 1977\n"
+file.write(str(newrecord))
+newrecord ="Episode V, The Empire Strikes Back, 1981\n"
+file.write(str(newrecord))
+newrecord ="Episode VI, Thre Return of the Jedi, 1985\n"
+file.write(str(newrecord))
+newrecord ="Episode VII, The Force Awakens, 2015\n"
+file.write(str(newrecord))
+newrecord ="Episode VIII, the Last Jedi, 2017\n"
+file.write(str(newrecord))
+file.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Written by Nichola Lacey and copyright owned by (c) Nichola Wilkin Ltd. 2017
