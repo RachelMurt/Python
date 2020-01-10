bfed14e468b8832704813ca19dc42a3fc9a681e2
Rachel Murtagh <56913574+RachelMurt@users.noreply.github.com>
2020-01-10 09:56:00 +0000

    Delete a3.py

--- a3.py
+++
@@ -1,8 +0,0 @@
-mrOHaresFile=open("Text_File.txt","a")#here is the append code
-#now that the files created lets WRITE to it!
-mrOHaresFile.write("Top Three Songs according to Mr O\n")
-mrOHaresFile.write("at 1... Georgia - Ray Charles\n")
-mrOHaresFile.write("at 2... U2 - One Tree Hill\n")
-mrOHaresFile.write("at 1... ACDC - Shook me all night long\n")
-# not really my top 3, but time to close the file, again!
-mrOHaresFile.close()
